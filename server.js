0aadf0d9a7d8bbdccd49fbcc0dfc697d45070f7e
yanagiya <extendcrisis@gmail.com>
2019-02-17 16:00:02 +0900

    make hello World test app

---
+++ server.js
@@ -0,0 +1,11 @@
+express = require("express");
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res){
+  res.send('Hello World')
+})
+app.listen(app.get('port'), function(){
+  console.log("Node testapp is runnging at localhost:" + app.get('port'))
+})
